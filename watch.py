a1499ae9c81bccddbe6e89a487cd9b22b08f917c
s4intpaul <160616332+s4intpaul@users.noreply.github.com>
2026-06-14 23:16:25 +1000

    Update watch.py
    *Added ticker count for manual ticker selection
    *still havent figured parameter issue in def a_t_w
    *program works well, trial with .AU flag for AUS stocks

--- watch.py
+++ watch.py
@@ -1,17 +1,45 @@
+import yfinance as yf
 
 watchlist ={}
 
+def ticker_count(count, tickers):
+    count = int(input(f"How many Tickers?: "))
+    if count > 10:
+        print("Exceeded Max Count(10). Try Again.")
+    elif count > 0 and len(tickers) < count:
+        for i in range(count):
+            add_to_watchlist(watchlist)
+
 def add_to_watchlist(watchlist):
-    ticker = input(f"Enter ticker: ").upper()
+    ticker: str = input(f"Enter ticker: ").upper()
     target = float(input(f"Enter target price for {ticker}: $"))
     watchlist[ticker] = {"target": target}
     print(f"Added {ticker} to watchlist at ${target:.2f}")
 
+def get_price(ticker):
+    stock = yf.Ticker(ticker)
+    return stock.info["currentPrice"]
+
+
 def main():
-    add_to_watchlist(watchlist)
-    add_to_watchlist(watchlist)
-    add_to_watchlist(watchlist)
-    print(watchlist)
+    ticker_count(0, watchlist)
+
+    for ticker in watchlist:
+        price = get_price(ticker)
+        print(f"{ticker}: {price:.2f}")
+
+    for ticker in watchlist:
+        price = get_price(ticker)
+        target = watchlist[ticker]["target"]
+
+        if price >= target:
+            print(f"{ticker}: ${price:.2f} TARGET ACQUIRED (target: {target:.2f})")
+        else:
+            diff = abs(target - price)
+            print(f"{ticker:} ${price} | ${diff:.2f} away from target ${target:.2f}")
 
 if __name__ == "__main__":
    main()
+
+
+
